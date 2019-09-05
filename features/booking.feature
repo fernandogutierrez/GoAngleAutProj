7037d91322188fc9f350c84ff2ff5cc9af96988f
Fernando Gutierrez <fernandogutierrez279@gmail.com>
2019-09-05 11:10:56 -0400

    Added new steps and fixed the existing ones

--- features/booking.feature
+++ features/booking.feature
@@ -5,9 +5,15 @@ Feature:
 
   Scenario: Make a booking
     Given I select 'London, England' as destination
-    And I select 'September 5, 2019' as date
+#    And I select 'September 5, 2019' as date
     And I select '09:00am' as starting time
-#    And I click on Search
+    And I click on 'Search'
+    And I select photographer 'tester'
+    And I click on 'Continue'
+    And I select package 'Two Hours Package'
+    And I click on 'Continue'
+    And I click on 'Proceed to Payment'
+
 #    And Select a photographer
 
 
--- support/step_definitions/my_steps.js
+++ support/step_definitions/my_steps.js
@@ -2,17 +2,33 @@ let { Given } = require('cucumber');
 
 Given(/^I open site '(.*)'$/,(siteUrl) => {
     browser.url(siteUrl);
-    browser.windowHandleMaximize();
+    browser.maximizeWindow();
 });
 
 Given(/^I select '(.*)' as destination$/,(destination) => {
-    browser.element("//input[@placeholder='Type your city']").set(destination)
+    $("//input[@placeholder='Type your city']").setValue(destination);
 });
 
-Given(/^I select '(.*)' as date/,(destination) => {
-    browser.element("//input[@placeholder='Select...']").set(destination)
+Given(/^I select '(.*)' as starting time/,(stTime) => {
+    $("//input[@placeholder='Select...']").setValue(stTime);
+    $("//h1[text()='Pack a Photographer']").click();
 });
 
-Given(/^I select '(.*)' as starting time$/,(destination) => {
-    browser.element("//button/span[text()='Search']").set(destination)
+Given(/^I select photographer '(.*)'$/,(photographer) => {
+    let photoXpath = `//h3[text()='${photographer}']/ancestor::div[@class='wrappers__DinamicDiv-sc-83cfqq-0 dTAlQV']/following-sibling::button/descendant::div[text()='Select Photographer']`;
+    $(photoXpath).click();
 });
+
+Given(/^I click on '(.*)'$/,(btnName) => {
+    $(`//button/descendant::*[text()='${btnName}']`).click();
+    browser.pause(10000)
+});
+
+Given(/^I select package '(.*)'$/,(packageName) => {
+    $(`//h4[text()='${packageName}']/following-sibling::div/button/span[text()='Select']`).click();
+});
+
+
+
+
+
